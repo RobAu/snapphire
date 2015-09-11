9772bde52b0011507b9aa7020aff6a8480e5f83d
d2a-raudenaerde <rob.audenaerde@data2act.com>
2015-09-11 14:19:38 +0200

    more stuff

--- snappcar/src/main/java/org/audenaerde/App.java
+++ snappcar/src/main/java/org/audenaerde/App.java
@@ -1,6 +1,8 @@
 package org.audenaerde;
 
 import java.io.IOException;
+import java.io.Serializable;
+import java.lang.reflect.Type;
 import java.text.DateFormat;
 import java.text.DecimalFormat;
 import java.text.ParseException;
@@ -8,12 +10,17 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Locale;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
 
 /**
@@ -38,67 +45,25 @@ public class App
 			return Double.compare(totalPrice, o.totalPrice);
 		}
 	}
-	static class Car
-	{
-		public Car(String carurl, String price)
-		{
-			this.url = carurl;
-			try
-			{
-				this.basePrice = DecimalFormat.getInstance(Locale.GERMAN).parse(price.replace("€ ","")).doubleValue();
-			} catch (ParseException e)
-			{
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}
+	
 
-		@Override
-		public String toString()
-		{
-			return "Car [url=" + url + ", name=" + name + ", basePrice=" + basePrice + ", pricePerKm=" + pricePerKm
-					+ ", kmFree=" + kmFree + "]";
-		}
-
-
-		String url;
-		String name;
-		String id;
-		String owner;
-		double basePrice;
-		double pricePerKm;
-		int kmFree;
-		
-		public double getPricePerKm()
-		{
-			return pricePerKm;
-		}
-
-		public void setPricePerKm(double pricePerKm)
-		{
-			this.pricePerKm = pricePerKm;
-		}
-
-		public int getKmFree()
-		{
-			return kmFree;
-		}
-
-		public void setKmFree(int kmFree)
-		{
-			this.kmFree = kmFree;
-		}
-	}
-
+	
 	public static void main(String[] args) throws IOException
 	{
+		
+		double mylat  = 52.0914852;
+		double mylong =  5.12342760000001;
+
+
+		System.out.println(Haversine.haversine(52.0914852, 5.12342760000001, 52.08846, 5.12143666666667));
+		
 		String startDate = "02-10-2015";
 		String startTime = "09:00".replace(":", "%3A"  );
 		String endDate = "04-10-2015";
 		String endTime = "22:00".replace(":", "%3A"  );
 		int kms = 400;
 		
-		String url = "https://www.snappcar.nl/search.aspx?lat=52.0914852&lng=5.12342760000001";
+		String url = "https://www.snappcar.nl/search.aspx?lat="+ mylat+ " &lng=" + mylong;
 		url+="&rl=&ps="+startDate+"&ps="+startTime+"&pe="+endDate+"&pe="+endTime+"&mip=&map=&miy=&may=&cm=&cb=&pis=undefined&o=Distance&view=l&rr=10&cci=&fkd=Geen%20selectie...&fo=0";
 		url+="&bt=stationwagen";
 		List<Car> cars = new ArrayList<Car>();
@@ -106,14 +71,7 @@ public class App
 		{
 			System.out.println("Getting cars page:" + i);
 			cars.addAll(getCars(url, i));
-			try
-			{
-				Thread.sleep(1000);
-			} catch (InterruptedException e)
-			{
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
+			sleep(1000);
 
 		}
 		cars.stream().forEach(car -> getDetails(car));
@@ -122,10 +80,22 @@ public class App
 		Collections.sort(optionalHires);
 		for (Hire h : optionalHires)
 		{
-			System.out.println(h.totalPrice + " -- " + h.c.url);
+			System.out.println(h.c.owner + " == " + h.totalPrice + " -- " + Haversine.haversine(mylat, mylong, h.c.latitude, h.c.longitude)  + "km -- " + h.c.url);
 		}
 		
 		
+	}
+
+	private static void sleep(long ms)
+	{
+		try
+		{
+			Thread.sleep(ms);
+		} catch (InterruptedException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 	private static List<Car> getCars(String url, int i) throws IOException
@@ -139,18 +109,48 @@ public class App
 		{
 			String carurl = elem.attr("data-href");
 			String price = elem.select("div[class=price h1]").text();
-			cars.add(new Car(carurl, price));
+			String owner = elem.select("img[class=owner-image]").attr("title");
+			Car c = new Car(carurl, price);
+			c.setOwner(owner);
+			cars.add(c);
+			
 		}
+		sleep(500);
 		return cars;
 	}
+	
+	public static double parseDouble(String s) throws ParseException
+	{
+		return DecimalFormat.getInstance(Locale.GERMAN).parse(s).doubleValue();
+	}
+	
 
 	private static void getDetails(Car car)
 	{
+		System.out.println("Getting details of:" + car.url);
+		
 		try
 		{
 			String url = "https://www.snappcar.nl" + car.url;
 			Document doc = Jsoup.connect(url).get();
 
+		
+			Pattern carDetailsVars  = Pattern.compile("snappCar.detailMapsVars = \\{.*carData..(.*?)\\};");
+			Matcher m =carDetailsVars.matcher(doc.toString().replace("\n", "")); 
+			if (m.find())
+			{
+				String array = m.group(1);
+				Type collectionType = new TypeToken<List>(){}.getType();
+				List items = new Gson().fromJson(array, collectionType);
+				car.latitude = (double) items.get(1);
+				car.longitude = (double) items.get(2);
+			}
+
+//			Elements dis = doc.select("a[href=#map]").select("li");
+//			double distance = parseDouble(dis.text());
+//			
+//			car.setDistance(distance);
+			
 			Elements kmdetail = doc.select("li:contains(kilometers)");
 
 			String kmdetailstr = kmdetail.text();
@@ -159,6 +159,7 @@ public class App
 			System.out.println(car);
 		} catch (Exception e)
 		{
+			e.printStackTrace();
 		}
 	}
 	
@@ -178,5 +179,14 @@ public class App
 			e.printStackTrace();
 		} //100 vrije kilometers per dag (€ 0,20 per extra km)
 		return 0;
+	}
+	
+	public static String getCarData()
+	{
+	     String s = "   snappCar.detailMapsVars = {\n"+
+	           "   carData: ['Seat Exeo Stationwagon', 52.0946, 5.12969033333333, 1, '323b1b82-2090-4c4c-9ccb-e9b08ef2bc85', '47,50', null, 0, 0, 0, '/image.aspx?ii=68429418-cbfe-4f14-bdcd-339c6a36240c&w=38']\n"+
+	              " };";
+	     
+	     return s;
 	}
 }
---
+++ snappcar/src/main/java/org/audenaerde/Car.java
@@ -0,0 +1,71 @@
+package org.audenaerde;
+
+import java.text.DecimalFormat;
+import java.text.ParseException;
+import java.util.Locale;
+
+public class Car
+{
+	public Car(String carurl, String price)
+	{
+		this.url = carurl;
+		try
+		{
+			this.basePrice = DecimalFormat.getInstance(Locale.GERMAN).parse(price.replace("€ ","")).doubleValue();
+		} catch (ParseException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public String toString()
+	{
+		return "Car [url=" + url + ", name=" + name + ", basePrice=" + basePrice + ", pricePerKm=" + pricePerKm
+				+ ", kmFree=" + kmFree + "]";
+	}
+
+		
+	String url;
+	String name;
+	String id;
+	String owner;
+
+
+
+	double basePrice;
+	double pricePerKm;
+
+	int kmFree;
+	public double latitude;
+	public double longitude;
+	public double getPricePerKm()
+	{
+		return pricePerKm;
+	}
+
+	public void setPricePerKm(double pricePerKm)
+	{
+		this.pricePerKm = pricePerKm;
+	}
+
+	public int getKmFree()
+	{
+		return kmFree;
+	}
+
+	public void setKmFree(int kmFree)
+	{
+		this.kmFree = kmFree;
+	}
+	public String getOwner()
+	{
+		return owner;
+	}
+
+	public void setOwner(String owner)
+	{
+		this.owner = owner;
+	}
+}
